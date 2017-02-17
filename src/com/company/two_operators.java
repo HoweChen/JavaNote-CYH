540df4b423f322b5517d31cd3d2d91a26ef20c51
SkyFather <cyh278889789@gmail.com>
2017-02-17 21:38:26 +0800

    update operators

---
+++ src/com/company/two_operators.java
@@ -0,0 +1,18 @@
+package com.company;
+
+/**
+ * Created by howechen on 2/17/17.
+ */
+public class two_operators {
+    public static void main(String[] args) {
+        int a = 5 / 2;
+        System.out.println(a); //2
+        float b = 5 / 2;
+        System.out.println(b); //2.0
+        double c = 5 / 2;
+        System.out.println(c); //2.0
+
+        byte d = 42;
+        System.out.println(~d + 1); //-42
+    }
+}
