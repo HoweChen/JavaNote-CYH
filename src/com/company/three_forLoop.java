a74ddba7fba0a270d9e62df00a24819438cd6d0d
SkyFather <cyh278889789@gmail.com>
2017-02-20 00:21:35 +0800

    update forLoop

--- src/com/company/three_forLoop.java
+++ src/com/company/three_forLoop.java
@@ -7,9 +7,10 @@ public class three_forLoop {
     public static void main(String[] args) {
         int[][] twoD = new int[3][5];
 
+        int count = 0;
         for (int[] x : twoD) {
             for (int y : x) {
-                System.out.println("Hello World");
+                System.out.println(++count + ": Hello World");
             }
         }
 
