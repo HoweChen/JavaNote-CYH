b874ffbfed5a22f4f9d843d39fee7e1459c57830
SkyFather <cyh278889789@gmail.com>
2017-02-20 00:34:24 +0800

    update breakLabel

---
+++ src/com/company/breakLabel.java
@@ -0,0 +1,22 @@
+package com.company;
+
+/**
+ * Created by howechen on 2/20/17.
+ */
+public class breakLabel {
+    public static void main(String[] args){
+        boolean t = true;
+        first:{
+            second:{
+                third:{
+                    if (t){
+                        System.out.println("Hello world then break");
+                        break second;
+                    }
+                }
+                System.out.println("This line won't be printed");
+            }
+            System.out.println("I am in the first label");
+        }
+    }
+}
