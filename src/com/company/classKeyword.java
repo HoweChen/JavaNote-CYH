b4251158baf50e7307967dfad09e74105a5d3e3e
SkyFather <cyh278889789@gmail.com>
2017-02-20 17:28:51 +0800

    update classKeyword

--- src/com/company/classKeyword.java
+++ src/com/company/classKeyword.java
@@ -3,5 +3,44 @@ package com.company;
 /**
  * Created by howechen on 2/20/17.
  */
+
+class Box {
+    double width;
+    double height;
+    double depth;
+    double vol;
+
+    public void calculateVol() {
+        this.vol = this.depth * this.width * this.height;
+    }
+}
+
 public class classKeyword {
+
+
+    public static void main(String[] args) {
+        Box myBox = new Box();
+        myBox.width = 10;
+        myBox.height = 20;
+        myBox.depth = 15;
+
+
+        myBox.calculateVol();
+        System.out.println(String.valueOf(myBox.vol)); //3000.0
+
+
+        //myBox and myBox2 would be pointed to the same object instance
+        Box myBox2 = myBox;
+        myBox2.vol = 0;
+        System.out.println(String.valueOf(myBox2.vol)); //0.0
+        System.out.println(String.valueOf(myBox.vol)); //0.0
+
+
+    }
+}
+
+class BoxDemo2 {
+    public static void main(String[] args) {
+        System.out.println("Test");
+    }
 }
