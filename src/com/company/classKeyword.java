4a3d3d75e46900034fc67a4b2b9f5c95eb50a528
SkyFather <cyh278889789@gmail.com>
2017-02-21 01:05:29 +0800

    update classKeyword with stack

--- src/com/company/classKeyword.java
+++ src/com/company/classKeyword.java
@@ -5,25 +5,77 @@ package com.company;
  */
 
 class Box {
+
     double width;
     double height;
     double depth;
     double vol;
 
-    void calculateVol() {
+    public Box(double width, double height, double depth) {
+        System.out.println("Constructing Box");
+        this.width = width;
+        this.height = height;
+        this.depth = depth;
+        this.calculateVol();
+    }
+
+    public void calculateVol() {
         this.vol = this.depth * this.width * this.height;
     }
 
-    void volume() {
+    public void volume() {
         System.out.println("Volume: " + this.vol);
     }
+}
+
+class Stack {
+    int[] stack;
+    int flag;
+
+    public Stack(int size) {
+        if (size <= 0) {
+            System.out.println("Cannot set a negative size of a stack");
+        } else {
+            this.stack = new int[size];
+            this.flag = 0;
+        }
+    }
+
+    public void push(int element) {
+        if (this.flag >= this.stack.length - 1) {
+            System.out.println("The stack is already full and the element cannot be pushed into it.");
+        } else {
+            this.stack[this.flag] = element;
+            ++this.flag;
+            System.out.println(element + "has been pushed");
+        }
+
+    }
+
+    public int pop(int element) {
+        if (this.flag < 0) {
+            System.out.println("The stack has been empty and cannot be popped");
+        } else {
+            return this.stack[flag--];
+        }
+        return element;
+    }
+
+    public void show_stack() {
+        for (int itr :
+                stack) {
+            System.out.println(itr);
+        }
+    }
+
+
 }
 
 public class classKeyword {
 
 
     public static void main(String[] args) {
-        Box myBox = new Box();
+        Box myBox = new Box(10, 20, 15);
         myBox.width = 10;
         myBox.height = 20;
         myBox.depth = 15;
@@ -48,5 +100,14 @@ public class classKeyword {
 class BoxDemo2 {
     public static void main(String[] args) {
         System.out.println("Test");
+    }
+}
+
+class stackDemo {
+    public static void main(String[] args) {
+        System.out.println("Stack Test Demo");
+
+        Stack stackTest = new Stack(5);
+        stackTest.show_stack();
     }
 }
