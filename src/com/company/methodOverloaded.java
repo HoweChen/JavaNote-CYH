f031a7b95e30573100909d637f8bb5fdcbce0391
SkyFather <cyh278889789@gmail.com>
2017-02-21 16:26:14 +0800

    update methodOverloaded

--- src/com/company/methodOverloaded.java
+++ src/com/company/methodOverloaded.java
@@ -3,8 +3,35 @@ package com.company;
 /**
  * Created by howechen on 2/21/17.
  */
+
+class Overloaded {
+    void test() {
+        System.out.println("No parameter");
+    }
+
+    void test(int oneP) {
+        System.out.println("One parameter");
+    }
+
+    void test(int oneP, int twoP) {
+        System.out.println("Two parameters");
+    }
+
+    void test(int oneP, int twoP, int threeP) {
+        System.out.println("Three parameters");
+    }
+}
+
 public class methodOverloaded {
     public static void main(String[] args) {
         System.out.println("Hello World");
+
+        Overloaded demo = new Overloaded();
+        demo.test();
+        demo.test(0);
+        demo.test(0, 0);
+        demo.test(0, 0, 0);
+
     }
 }
+
