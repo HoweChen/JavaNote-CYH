79304445c98af169ab2f183ee63565097af35e74
SkyFather <cyh278889789@gmail.com>
2017-02-21 16:22:57 +0800

    update methodOverloaded

---
+++ src/com/company/methodOverloaded.java
@@ -0,0 +1,10 @@
+package com.company;
+
+/**
+ * Created by howechen on 2/21/17.
+ */
+public class methodOverloaded {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
