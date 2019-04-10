355ca0e30cc9681f87f80215909bc69e02e85dd8
HoweChen <yuhao.howe.chen@gmail.com>
2019-04-10 11:22:00 +0800

    create Employee class and its test

---
+++ src/book/corejava/code/employee/Employee.java
@@ -0,0 +1,44 @@
+package book.corejava.code.employee;
+
+import java.time.LocalDate;
+
+public class Employee {
+    private String name;
+    private Double salary;
+    private LocalDate hireDay;
+
+    Employee(String name, double salary, int year, int month, int day) {
+        this.name = name;
+        this.salary = salary;
+        this.hireDay = LocalDate.of(year, month, day);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
+
+    public LocalDate getHireDay() {
+        return hireDay;
+    }
+
+    public void setHireDay(LocalDate hireDay) {
+        this.hireDay = hireDay;
+    }
+
+    public void raiseSalary(double byPercent) {
+        double raise = salary * byPercent / 100;
+        salary += raise;
+    }
+}
