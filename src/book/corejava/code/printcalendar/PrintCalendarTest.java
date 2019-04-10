9e79f330f23453f11538b779402b4eea88866c74
HoweChen <yuhao.howe.chen@gmail.com>
2019-04-10 10:49:46 +0800

    add PrintCalendarTest

---
+++ src/book/corejava/code/printcalendar/PrintCalendarTest.java
@@ -0,0 +1,10 @@
+package book.corejava.code.printcalendar;
+
+import book.corejava.code.printcalendar.PrintCalendar;
+
+public class PrintCalendarTest {
+    public static void main(String[] args) {
+        PrintCalendar test = new PrintCalendar();
+        test.print();
+    }
+}
